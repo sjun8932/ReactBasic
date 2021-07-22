7b724b68affacba32448820684c1b49c9da81354
unknown <sjun8932@gmail.com>
2021-07-22 09:14:38 +0900

    2-6. 구구단 웹팩으로 빌드하기 (클래스 문법)

---
+++ GuGuDan/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports={
+    name: 'gugudan-setting',
+    mode: 'development',
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    entry: {
+        app: ['./client'],
+    },
+
+    module: {
+        rules: [{
+            test:/\.jsx?/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env','@babel/preset-react'],
+                plugins: [],
+            }
+        }],
+    },
+
+
+    output: {
+        path: path.join(__dirname,'dist'),
+        filename: 'app.js'
+    },
+};
