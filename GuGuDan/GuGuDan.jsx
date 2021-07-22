1cf413e8bf476fb2468281f55ea40f2ba21d64ce
unknown <sjun8932@gmail.com>
2021-07-22 09:36:42 +0900

    2-6. 구구단 웹팩으로 빌드하기 (hooks 방식)

--- GuGuDan/GuGuDan.jsx
+++ GuGuDan/GuGuDan.jsx
@@ -1,48 +1,42 @@
 const React = require('react');
-const {Component} = React;
+const { useState, useRef } = React;
 
-class GuGuDan extends Component {
-                
-    state = {
-        first: Math.ceil(Math.random() * 9),
-        second: Math.ceil(Math.random() * 9),
-        value: '',
-        result: '',
+const GuGuDan = () => {
+    const [first, setFirst] = useState(Math.ceil(Math.random() * 9));
+    const [second, setSecond] = useState(Math.ceil(Math.random() * 9));
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
+    const inputEl = useRef(null);
+
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+        if(parseInt(value) === first * second ){
+            setResult('정답');
+            setFirst(Math.ceil(Math.random() * 9));
+            setSecond(Math.ceil(Math.random() * 9));
+            setValue('');
+            inputEl.current.focus();
+        } else {
+            setResult('땡');
+            setValue('');
+            inputEl.current.focus();
+        }
     };
-
-onSubmit = (e) => {
-    e.preventDefault();
-    if (parseInt(this.state.value) === this.state.first * this.state.second) {
-        this.setState({
-            result: `정답은 ${this.state.value}`,
-            first: Math.ceil(Math.random() * 9),
-            second: Math.ceil(Math.random() * 9),
-            value: '',
-        });
-    } else {
-        this.setState({
-            result: '땡',
-            value: '',
-        });
-    }
-};
-
-onChange = (e) => {
-    this.setState({ value: e.target.value });
-};
-
-render() {
     return (
         <React.Fragment>
-            <div>{this.state.first} 곱하기 {this.state.second}는?</div>
-            <form onSubmit={this.onSubmit}>
-                <input type="number" value={this.state.value} onChange={this.onChange} />
+            <div>{first} 곱하기 {second}는?</div>
+            <form onSubmit={onSubmitForm}>
+                <input
+                    ref={inputEl}
+                    type="number"
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                />
                 <button>입력!</button>
             </form>
-            <div>{this.state.result}</div>
+            <div id="result">{result}</div>
         </React.Fragment>
-    );
-}
+    )
 }
 
 module.exports = GuGuDan;
