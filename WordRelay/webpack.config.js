7ce904486cdce238659fd1275afd773c4f88650c
unknown <sjun8932@gmail.com>
2021-07-23 15:23:48 +0900

    2-7. @babel/preset-env와 plugins

--- WordRelay/webpack.config.js
+++ WordRelay/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports={
     name: 'word-relay-setting',
@@ -17,12 +18,13 @@ module.exports={
             test:/\.jsx?/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env','@babel/preset-react'], 
-            }
+                presets: [['@babel/preset-env', {targets: {browsers: ['> 1% in KR', 'last 2 chrome versions'],},debug: true,}] ,'@babel/preset-react'],
+                plugins: [],
+            },
         }],
     },
-
-
+    plugins: [new webpack.LoaderOptionsPlugin({ debug: true })
+    ],
     output: {
         path: path.join(__dirname,'dist'),
         filename: 'app.js'
