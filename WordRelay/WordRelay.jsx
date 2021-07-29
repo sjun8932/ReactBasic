bbedd23469c2c5a2cd58a37de60da0d1d255726f
unknown <sjun8932@gmail.com>
2021-07-29 10:42:43 +0900

    2-10. 끝말잇기 Hooks로 전환하기

--- WordRelay/WordRelay.jsx
+++ WordRelay/WordRelay.jsx
@@ -3,7 +3,7 @@ const { useState, useRef} = React;
 
 const WordRelay = () => {
     
-    const [word, setWord] = useState('제시어 : 시작');
+    const [word, setWord] = useState('시작');
     const [value, setValue] = useState('');
     const [result, setResult] = useState('');
     const inputRef = useRef(null);
@@ -13,15 +13,11 @@ const WordRelay = () => {
     
         e.preventDefault();
         if(word[word.length-1] === value[0]){
-                var element = document.body;
-                element.style.backgroundColor = "green";   
                 setResult('정답입니다'),
                 setWord(value),
                 setValue(''), 
                 inputRef.current.focus(); 
         } else{
-                var element = document.body;
-                element.style.backgroundColor = "red";   
                 setResult('틀렸습니다'),
                 setValue(''),
                 inputRef.current.focus();
@@ -34,11 +30,10 @@ const WordRelay = () => {
     
         return (
             <>
-            <div style={{color:"black", position:"absolute", top:"50%", left:"50%" ,transform: "translate(-50%, -50%)" , textAlign:"center"}}>
-                <div style ={{textAlign:"center"}}>{word}</div>
+            <div>
+                <div>{word}</div>
                 <form onSubmit = {onSubmitForm} >
-                    <label htmlFor="sexy">😂😁😉😘😊끝말잇기 조져부러😎😒🤣😒😂</label>
-                    <br></br>
+                    <label htmlFor="sexy">끝말잇기</label>
                     <input id="sexy" ref={inputRef} value={value} onChange={onChangeInput} />
                     <button>클릭!</button>
                 </form>
