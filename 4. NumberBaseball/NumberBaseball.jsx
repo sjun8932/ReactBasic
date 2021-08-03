80cf2379538b84ce8315c9e97074c5c127b125a3
SANGJUNPARK <sjun8932@gmail.com>
2021-08-03 18:30:00 +0900

    3-6. 숫자야구 만들기

--- 4. NumberBaseball/NumberBaseball.jsx
+++ 4. NumberBaseball/NumberBaseball.jsx
@@ -1,53 +1,95 @@
 import React, {Component} from 'react'; // 1. import React from 'react' + 2. import {Component} from 'react';
 import Try from './Try';
 
-function getNumbers () {
-
+function getNumbers() { // 숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
+    const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const array = [];
+    for (let i = 0; i < 4; i += 1) {
+        const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        array.push(chosen);
+    }
+    return array;
 }
 
 class NumberBaseball extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
             result: '',
             value: '',
-            answer: getNumbers(),
+            answer: getNumbers(), // ex : [1,3,5,7]
             tries: [],
-        }
+        };
+        console.log(this.state.answer);
         this.onSubmitForm = this.onSubmitForm.bind(this);
         this.onChangeInput = this.onChangeInput.bind(this);
     }
 
-
-    onSubmitForm (e) {
-
-    };
-    
-    onChangeInput (e)  {
-
+    onChangeInput(e) {
+        console.log(this.state.answer);
+        this.setState({
+            value: e.target.value,
+        });
     };
 
-    StayC = [
-        {member : '시은' , feature: '메인보컬'},
-        {member : '수민' , feature: '리더, 래퍼, 서브보컬'},
-        {member : '세은' , feature: '서브보컬'},
-        {member : '윤' , feature: '리드보컬'},
-        {member : '재이' , feature: '랩, 서브보컬'},
-        {member : '아이사' , feature: '리드보컬'}
-    ]
+    onSubmitForm(e) {
+        e.preventDefault();
+        if (this.state.value === this.state.answer.join('')) {
+            this.setState({
+                result: '홈런',
+                tries: [...this.state.tries, {try: this.state.value, result: '홈런!'}],
+            })
+            alert('게임을 다시 시작합니다!');
+            this.setState({
+                value: '',
+                answer: getNumbers(),
+                tries: []
+            });
+        } else {
+            const answerArray = this.state.value.split('').map((v) => parseInt(v));
+            let strike = 0;
+            let ball = 0;
+            if (this.state.tries.length >= 9) { // 10번 이상 틀렸을 때
+                this.setState({
+                    result: `10번 넘게 틀려서 실패! 답은 ${this.state.answer.join(',')}이었습니다.`,
+                });
+                alert('게임을 다시 시작합니다!');
+                this.setState({
+                    value: '',
+                    answer: getNumbers(),
+                    tries: []
+                });
+            } else {
+                for (let i = 0; i < 4; i += 1) {
+                    if (answerArray[i] === this.state.answer[i]) {
+                        strike += 1;
+                    } else if (this.state.answer.includes(answerArray[i])) {
+                        ball += 1;
+                    }
+                }
+                this.setState({
+                    tries: [...this.state.tries, {try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다.`}],
+                    value: '',
+                })
+            }
+        }
+    }
 
-    render () {
+
+    render() {
         return (
-            < >
+            <>
                 <h1>{this.state.result}</h1>
                 <form onSubmit={this.onSubmitForm}>
-                    <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
+                    <input maxLength={4} value={this.state.value} onChange={this.onChangeInput}/>
                 </form>
                 <div>시도: {this.state.tries.length}</div>
                 <ul>
-                    {
-                        this.StayC.map( (v,i) => {
-                        return(<Try key={v.member + v.feature} value={v} index={i}/>);
+                    {this.state.tries.map((v, i) => {
+                        return (
+                            <Try key={`${i + 1}차 시도:`} tryInfo={v} index={i}/>
+                        );
                     })}
                 </ul>
             </>
--- 4. NumberBaseball/Try.jsx
+++ 4. NumberBaseball/Try.jsx
@@ -1,14 +1,16 @@
+
 import React  from 'react';
 import { Component } from 'react';
 
 class Try extends Component {
-
-    render(){
-        return(
-            <li><b style={{color:"#98FF98"}}>"{this.props.value.member}"</b>은 {this.props.value.feature} {this.props.index}번</li>
+    render() {
+        return (
+            <li>
+                <div>{this.props.tryInfo.try}</div>
+                <div>{this.props.tryInfo.result}</div>
+            </li>
         );
-    };
+    }
 }
-
 
 export default Try;
