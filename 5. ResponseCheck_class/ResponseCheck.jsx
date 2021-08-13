742dbf1002a43edef8b3fe41f80f7c87602327d6
SANGJUN <sjun8932@gmail.com>
2021-08-13 16:58:47 +0900

    4-2. setTimeout에 넣어 반응속도 체크

--- 5. ResponseCheck_class/ResponseCheck.jsx
+++ 5. ResponseCheck_class/ResponseCheck.jsx
@@ -4,13 +4,45 @@ import {Component} from 'react';
 class ResponseCheck extends Component {
 
     state = {
-        state: 'waiting',
+        state: 'waiting', //aqua
         message: '클릭해서 시작하세요',
         result: [],
     };
 
-    onClickScreen = () => {
+    timeout;
+    startTime;
+    endTime;
 
+    onClickScreen = () => {
+        const{ state, message, result } = this.state; // 매번 this.state가 쓰기 싫으면 미리미리 구조분해 할당을 해놓자....
+        if ( state === 'waiting' ){
+            this.setState({
+                state: 'ready', // red
+                message: '초록색이 되면 클릭하세요',
+            });
+            this.timeout = setTimeout(( )=> { // Q.) this.timeout 을 만들어주는 이유는?
+                this.setState({
+                    state:'now', // greenYellow
+                    message:'지금클릭',
+                });
+                this.startTime = new Date();
+            }, Math.floor(Math.random() * 1000) + 2000);
+        } else if (state === 'ready'){// 성급하게 클릭
+            clearTimeout(this.timeout);
+            this.setState({
+                state: 'waiting', //aqua
+                message: '너무 성급하게 누루셨어요 😤😤'
+            })
+        } else if (state === 'now'){ // 반응속도 체크
+            this.endTime = new Date();
+            this.setState((prevState) => {
+                return {
+                    state: 'waiting',
+                    message: '클릭해서 시작하세요',
+                    result: [...prevState.result , this.endTime - this.startTime],
+                };
+            });
+        }
     };
 
     render(){
@@ -23,9 +55,6 @@ class ResponseCheck extends Component {
             </>
         );
     }
-
-
-
 }
 
 export default ResponseCheck;
