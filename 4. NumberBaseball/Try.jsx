b63ec3db66c594798abc7810eb3c4d01a8c6e3c1
SANGJUN <sjun8932@gmail.com>
2021-08-13 11:57:29 +0900

    3-13. props와 state 연결하기

--- 4. NumberBaseball/Try.jsx
+++ 4. NumberBaseball/Try.jsx
@@ -1,12 +1,22 @@
 
-import React  from 'react';
+import React from 'react';
 import { memo } from 'react';
+import { useState } from "react";
 
 const Try = memo ( ({ tryInfo }) => {
+    // tryInfo.try = 'hello' => 부모로 받은 props의 경우 자식이 직접 바꾸면 안좋다. 왜? 부모에게도 영향을 미치니까
+    // 그런데 실무에서는 불가피하게 바꾸어야 할 경우가 있다.
+    // 그럴 때에는 props를 state로 만들어주고 그 state를 변경해준다
+    const [result, setResult] = useState( tryInfo.result );
+
+    const onClick = () => {
+        setResult('부모로 받은 props 변경 🤣🤣');
+    };
+
     return (
         <li>
             <div>{tryInfo.try}</div>
-            <div>{tryInfo.result}</div>
+            <div onClick = {onClick}>{result}</div>
         </li>
     );
 });
--- 4.5. NumberBaseball_class/Try.jsx
+++ 4.5. NumberBaseball_class/Try.jsx
@@ -2,6 +2,13 @@ import React  from 'react';
 import { Component } from 'react';
 
 class Try extends Component {
+
+    state = {
+        result: this.props.result,
+        try: this.props.try,
+    };
+    // 클래스에서 자식 컴포넌트가 state를 변경하고 싶으면 state를 재정의 해주면 된다.
+
     render() {
         return (
             <li>
