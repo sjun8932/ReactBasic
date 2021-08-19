ae0c80c0dd883a23bba4c8c0e3dcaeb0b08e81d7
SANGJUN <sjun8932@gmail.com>
2021-08-19 10:44:00 +0900

    6-1. 로또추첨기 컴포넌트

---
+++ 7. Lotto/Lotto.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Component} from "react";
+import Ball from './Ball';
+
+function getWinNumbers(){
+    console.log('getWinNumbers');
+    const candidate = Array(45).fill().map( (v,i) => i+1 );
+    const shuffle = [];
+    while (candidate.length > 0) {
+        shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
+    }
+    const bonusNumber = shuffle[shuffle.length -1];
+    const winNumbers = shuffle.slice(0,6).sort( (next,prev) => prev - next);
+    console.log(winNumbers);
+    return [...winNumbers, bonusNumber];
+}
+
+class Lotto extends Component {
+    state = {
+        getWinNumbers: getWinNumbers(),
+        winBalls: [],
+        bonus: null,
+        redo: false,
+    }
+    render() {
+        const { winBalls , bonus, redo } = this.state;
+        return (
+            <>
+                <div>당첨 숫자</div>
+                <div id="결과창">
+                    {winBalls.map( (v) => <Ball key={v} number={v} />)}
+                </div>
+                <div>보너스다!</div>
+                {bonus && <Ball number={bonus} />}
+                <button onClick={redo ? this.onClickRedo: () => {}}>한번 더!</button>
+            </>
+        )
+    }
+}
+
+export default Lotto;
